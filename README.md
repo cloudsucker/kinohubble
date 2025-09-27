9db9099a87f1f37e650e142909c29d64d1493d03
cls <48602112+maxfraid@users.noreply.github.com>
2025-09-27 14:43:21 +0300

    add python-venv check

--- README.md
+++ README.md
@@ -2,13 +2,20 @@
 
 ### 1. Клонируем проект
 
+```bash
 sudo git clone -b production https://github.com/cloudsucker/kinohubble.git /opt/kinohubble
+```
 
 ### 2. Кладём deploy.sh
 
+```bash
 cd /opt/kinohubble
-chmod +x deploy.sh
+sudo chmod +x deploy.sh
+```
 
 ### 3. Первый запуск (сам создаст сервис и запустит)
 
-./deploy.sh
+```bash
+sudo apt install python3.12-venv
+sudo ./deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,47 @@
 #!/bin/bash
 set -e # exit immediately if a command exits with a non-zero status
 
+
 # VARIABLES
 APP_NAME="kinohubble"
 PROJECT_DIR="/opt/$APP_NAME"
 PYTHON_BIN="/usr/bin/python3"
 SYSTEMD_FILE="/etc/systemd/system/$APP_NAME.service"
 
+
 cd "$PROJECT_DIR"
 
+
+# ===== UPDATE CODE =====
 echo "Updating codebase from GitHub..."
 git pull origin production
 
+
+# ===== VIRTUAL ENV =====
 echo "Checking for virtual environment..."
-if [ ! -d ".venv" ]; then
-  $PYTHON_BIN -m venv .venv
+if [ ! -d ".venv" ] || [ ! -f ".venv/bin/activate" ]; then
+  echo "Creating virtual environment..."
+
+  # Устанавливаем python3-venv, если его нет
+  if ! python3 -m venv --help >/dev/null 2>&1; then
+    echo "python3-venv is missing. Installing..."
+    sudo apt update
+    sudo apt install -y python3-venv
+  fi
+
+  python3 -m venv .venv
 fi
 
+
+# ===== INSTALL REQUIREMENTS =====
 echo "Installing requirements..."
 source .venv/bin/activate
 pip install --upgrade pip
 pip install -r requirements.txt
 deactivate
 
+
+# ===== SYSTEMD UNIT =====
 if [ ! -f "$SYSTEMD_FILE" ]; then
   echo "Creating systemd unit..."
   sudo tee $SYSTEMD_FILE > /dev/null <<EOL
